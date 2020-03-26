c8bd2bc174b2ae57d2ee4fea35abf5c133a45558
Umer Khan <umar_s66@outlook.com>
2020-03-26 16:50:49 +0500

    Initial Commit

---
+++ Calculator_Project_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator_Project_2
+//
+//  Created by Umer Khan on 26/03/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
