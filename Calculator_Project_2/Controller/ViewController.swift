93a8735aa88013614a0e985e7fc5d41fc23a83ec
Umer Khan <umar_s66@outlook.com>
2020-03-27 16:22:08 +0500

    Update ViewController.swift
    Refactor Appearance Code

--- Calculator_Project_2/Controller/ViewController.swift
+++ Calculator_Project_2/Controller/ViewController.swift
@@ -9,124 +9,119 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - UI Components
+    @IBOutlet weak var tappedButtonsTextView: UITextView!
+    @IBOutlet weak var calculatedAnswerTextView: UITextView!
+    @IBOutlet var allButtons: [UIButton]!
+    
     // MARK: - Logic Supporting Properties
     var numberOnScreen: Double = 0.0
     var previousNumber: Double = 0
     var operation: Double = 0.0
     var isPerfromingOperation: Bool = false
     
-    
-    
-    
-    // MARK: - UI Components
-    @IBOutlet weak var tappedButtonsTextView: UITextView!
-    @IBOutlet weak var calculatedAnswerTextView: UITextView!
-    @IBOutlet var allButtons: [UIButton]!
-    
-    
-    // MARK: - Supporting Properties
+    // MARK: - UI Supporting Properties
     var isDarkMode: Bool = false
     let whiteAppearanceMoonIcon = #imageLiteral(resourceName: "moon")
     let darkAppearanceSunIcon = #imageLiteral(resourceName: "sun")
 
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+    //MARK: - App Appearance
     @IBAction func appearanceButtonTapped(_ sender: UIButton) {
-        //TODO: - Change Icon
         isDarkMode = !isDarkMode
-        
         if !isDarkMode && sender.tag == 19 {
             sender.setImage(whiteAppearanceMoonIcon, for: .normal)
-            // set view white
-            view.backgroundColor = .white
-            UIApplication.shared.statusBarStyle = .default
-            
-            // answer textView color
-            calculatedAnswerTextView.textColor = UIColor(named: "output_text_color")
-            calculatedAnswerTextView.backgroundColor = .white
-            
-            // tapped textView color
-            tappedButtonsTextView.textColor = UIColor(named: "tappedtext_color")
-            tappedButtonsTextView.backgroundColor = .white
-            
-            //MARK: - Buttons color for whiteBG
-            for button in allButtons {
-                //TODO: = color change
-                switch button.tag {
-                    case 0...9:
-                        button.backgroundColor = UIColor(named: "whiteBG_numbuttons_color")
-                        button.titleLabel?.textColor = UIColor(named: "output_text_color")
-                    case 10...16:
-                        button.backgroundColor = UIColor(named: "whiteBG_opbuttons_green")
-                        button.titleLabel?.textColor = UIColor(named: "whiteBG_opbuttons_text_color")
-                    case 17:
-                        button.backgroundColor = UIColor(named: "whiteGB_equal_red")
-                        button.titleLabel?.textColor = UIColor(named: "whiteBG_equal_text_color")
-                    case 18:
-                        button.backgroundColor = UIColor(named: "whiteBG_numbuttons_color")
-                        button.titleLabel?.textColor = UIColor(named: "output_text_color")
-                    default:
-                        button.backgroundColor = .black
-                        button.titleLabel?.textColor = .white
-                }
-            }
+            colorsForLightMode()
         }
         else if isDarkMode && sender.tag == 19 {
             sender.setImage(darkAppearanceSunIcon, for: .normal)
-            view.backgroundColor = .black
-            //setNeedsStatusBarAppearanceUpdate()
-            UIApplication.shared.statusBarStyle = .lightContent
-            
-            
-            
-            // answer textView Color
-            calculatedAnswerTextView.backgroundColor = .black
-            calculatedAnswerTextView.textColor = .white
-            
-            // tapped textview color
-            tappedButtonsTextView.backgroundColor = .black
-            tappedButtonsTextView.textColor = UIColor(named: "blackBG_tapped_text_color")
-            
-            //MARK: - Buttons Color for DarkMode
-            for button in allButtons {
-                //TODO: = color change
-                switch button.tag {
-                    case 0...9:
-                        button.backgroundColor = .black
-                        button.titleLabel?.textColor = UIColor(named: "blackBG_tapped_text_color")
-                    case 10...16:
-                        button.backgroundColor = UIColor(named: "blackBG_opbuttons_color")
-                        button.titleLabel?.textColor = UIColor(named: "blackBG_opbuttons_text_color")
-                    case 17:
-                        button.backgroundColor = UIColor(named: "blackBG_equalbutton_color")
-                        button.titleLabel?.textColor = UIColor(named: "blackBG_equaluttons_text_color")
-                    case 18:
-                        button.backgroundColor = .black
-                        button.titleLabel?.textColor = UIColor(named: "blackBG_tapped_text_color")
-                    default:
-                        button.backgroundColor = .white
-                        button.titleLabel?.textColor = .black
-                }
-            }
+            colorsForDarkMode()
         }
     }
     
-    
+    // MARK: - Numbers Action
     @IBAction func numbersTapped(_ sender: UIButton) {
         
-        
-        
-        
-       
-        
     }
+    // MARK: - Operations Action
     @IBAction func operationsButtonsTapped(_ sender: UIButton) {
+        if sender.tag == 10 { // Clear Button
+            calculatedAnswerTextView.text = ""
+            tappedButtonsTextView.text = ""
+        }
         
     }
     
+}
+
+
+extension ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // TODO: - LightMode
+    func colorsForLightMode() {
+        
+        UIApplication.shared.statusBarStyle = .default
+        view.backgroundColor = .white
+        
+        calculatedAnswerTextView.textColor = UIColor(named: "output_text_color")
+        calculatedAnswerTextView.backgroundColor = .white
+        
+        tappedButtonsTextView.textColor = UIColor(named: "tappedtext_color")
+        tappedButtonsTextView.backgroundColor = .white
+        
+        for button in allButtons {
+            switch button.tag {
+                case 0...9:
+                    button.backgroundColor = UIColor(named: "whiteBG_numbuttons_color")
+                    button.titleLabel?.textColor = UIColor(named: "output_text_color")
+                case 10...16:
+                    button.backgroundColor = UIColor(named: "whiteBG_opbuttons_green")
+                    button.titleLabel?.textColor = UIColor(named: "whiteBG_opbuttons_text_color")
+                case 17:
+                    button.backgroundColor = UIColor(named: "whiteGB_equal_red")
+                    button.titleLabel?.textColor = UIColor(named: "whiteBG_equal_text_color")
+                case 18:
+                    button.backgroundColor = UIColor(named: "whiteBG_numbuttons_color")
+                    button.titleLabel?.textColor = UIColor(named: "output_text_color")
+                default:
+                    button.backgroundColor = .black
+                    button.titleLabel?.textColor = .white
+            }
+        }
+    }
+    // TODO: - DarkMode
+    func colorsForDarkMode() {
+        
+        UIApplication.shared.statusBarStyle = .lightContent
+        view.backgroundColor = .black
+        
+        calculatedAnswerTextView.backgroundColor = .black
+        calculatedAnswerTextView.textColor = .white
+        
+        tappedButtonsTextView.backgroundColor = .black
+        tappedButtonsTextView.textColor = UIColor(named: "blackBG_tapped_text_color")
+        
+        for button in allButtons {
+            switch button.tag {
+                case 0...9:
+                    button.backgroundColor = .black
+                    button.titleLabel?.textColor = UIColor(named: "blackBG_tapped_text_color")
+                case 10...16:
+                    button.backgroundColor = UIColor(named: "blackBG_opbuttons_color")
+                    button.titleLabel?.textColor = UIColor(named: "blackBG_opbuttons_text_color")
+                case 17:
+                    button.backgroundColor = UIColor(named: "blackBG_equalbutton_color")
+                    button.titleLabel?.textColor = UIColor(named: "blackBG_equaluttons_text_color")
+                case 18:
+                    button.backgroundColor = .black
+                    button.titleLabel?.textColor = UIColor(named: "blackBG_tapped_text_color")
+                default:
+                    button.backgroundColor = .white
+                    button.titleLabel?.textColor = .black
+            }
+        }
+    }
 }
 
