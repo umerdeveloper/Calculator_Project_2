06c7ffc53280ef992ea4f5181c0489e4c15813ae
Umer Khan <umar_s66@outlook.com>
2020-03-28 11:19:04 +0500

    operation struct added

---
+++ Calculator_Project_2/Controller/Operation.swift
@@ -0,0 +1,13 @@
+struct Operation {
+    
+    let clearScreen = 10
+    let setNagative = 11
+    let percentage = 12
+    let divide = 13
+    let multiply = 14
+    let subtract = 15
+    let add = 16
+    let result = 17
+    let dot = 18
+    
+}
